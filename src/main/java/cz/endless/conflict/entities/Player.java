7a9719618414b60652a031e7b3cfadd42d8602b7
dobeji1 <jitka.dobesova@hella.com>
2019-03-15 10:52:46 +0100

    New entity for player.

---
+++ src/main/java/cz/endless/conflict/entities/Player.java
@@ -0,0 +1,96 @@
+package cz.endless.conflict.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Created by dobeji1 on 15.03.2019.
+ */
+@Table(name = "PLAYER")
+@Entity
+public class Player implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Player() {
+    }
+
+    public Player(String nickname, String login, String password, String email) {
+        this.nickname = nickname;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "NICKNAME", nullable = false, unique = true)
+    private String nickname;
+
+    @Column(name = "LOGIN", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "EMAIL", nullable = false)
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(id, player.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
